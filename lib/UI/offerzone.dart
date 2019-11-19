4750f6abc593fff868e3c1990db8d34566196641
Ravdeep Singh <Ravdeepsingh22@gmail.com>
2019-11-19 21:11:49 +0530

    ui "10"

--- lib/UI/offerzone.dart
+++ lib/UI/offerzone.dart
@@ -41,85 +41,88 @@ class _OfferzoneState extends State<Offerzone> {
   ListView buildList(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
-        return GestureDetector(
-          onTap: (){
-            Navigator.push(context, SlideLeftRoute(page: ItemsOfferzone()));
-          },
-          child: Card(
-            child: Column(
-              children: <Widget>[
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 5),
+          child: GestureDetector(
+            onTap: (){
+              Navigator.push(context, SlideLeftRoute(page: ItemsOfferzone()));
+            },
+            child: Card(
+              child: Column(
+                children: <Widget>[
 
-                Container(
-                  width: double.infinity,
-                  height: 125,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    image: DecorationImage(
-                      image: AssetImage(
-                        'offerzone_01.png',
+                  Container(
+                    width: double.infinity,
+                    height: 125,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'offerzone_01.png',
 
-                      ),
-                      fit: BoxFit.cover
-                    )
+                        ),
+                        fit: BoxFit.cover
+                      )
+                    ),
                   ),
-                ),
 
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
 
-                      Text(
-                        Const.offerzone1, style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
-                      ),
-                      ),
+                        Text(
+                          Const.offerzone1, style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        ),
+                        ),
 
-                      SizedBox(height: 5,),
+                        SizedBox(height: 5,),
 
-                      Text(
-                        Const.offerzone2, style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 13,
-                      ),
-                      ),
-
-
-                      SizedBox(
-                        height: 15,
-                      ),
+                        Text(
+                          Const.offerzone2, style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 13,
+                        ),
+                        ),
 
 
-                      DottedBorder(
-                        color: Const.orange,
-                        strokeWidth: 1,
-                          borderType: BorderType.RRect,
-                          radius: Radius.circular(5),
-                        child: Container(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
-                            child: Text(
-                              'VEGETOSFIRST50',
-                              style: TextStyle(
-                                color: Const.orange,
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500
+                        SizedBox(
+                          height: 15,
+                        ),
+
+
+                        DottedBorder(
+                          color: Const.orange,
+                          strokeWidth: 1,
+                            borderType: BorderType.RRect,
+                            radius: Radius.circular(5),
+                          child: Container(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+                              child: Text(
+                                'VEGETOSFIRST50',
+                                style: TextStyle(
+                                  color: Const.orange,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500
+                                ),
                               ),
                             ),
-                          ),
+                          )
                         )
-                      )
 
 
-                    ],
-                  ),
-                )
+                      ],
+                    ),
+                  )
 
-              ],
-            )
+                ],
+              )
+            ),
           ),
         );
       },
